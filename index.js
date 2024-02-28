befb37dac1f700dbf1dc537356737fcd0b79753e
Abdullah Coban <111288151+AbdullahCoban28@users.noreply.github.com>
2024-02-28 21:56:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+async function analyzeGitHubProfile(username) {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    const userData = response.data;
+    
+    // Kullanıcı verilerini analiz etmek için gerekli işlemler yapılabilir
+    
+    console.log(userData);
+  } catch (error) {
+    console.error('GitHub profili alınamadı:', error.message);
+  }
+}
+
+module.exports = analyzeGitHubProfile;
